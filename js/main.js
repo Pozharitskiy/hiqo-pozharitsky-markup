a1a1ab5df9841012ff2b94de152baee81a608cd9
Pozharitskiy <33429207+Pozharitskiy@users.noreply.github.com>
2019-07-25 16:59:52 +0300

    Template experience

---
+++ js/main.js
@@ -0,0 +1,36 @@
+fetch('../data.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
+    createCellbox(myJson);
+  });
+
+fetch('../sliderData.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
+    giveSlides(myJson);
+  });
+
+function createCellbox(cellboxData) {
+  const rawTemplate = document.getElementById('cellbox').innerHTML;
+  const compiledTemplate = Handlebars.compile(rawTemplate);
+  const ourGeneratedHTML = compiledTemplate(cellboxData);
+  const cellboxContainer = document.querySelector('.cellbox');
+  console.log(cellboxContainer);
+  cellboxContainer.innerHTML = ourGeneratedHTML;
+}
+
+function giveSlides(slidesData) {
+  const sliderTemplate = document.getElementById('homeSlider').innerHTML;
+  const compiledTemplate = Handlebars.compile(sliderTemplate);
+  const ourGeneratedHTML = compiledTemplate(slidesData);
+  const slidesContainer = document.querySelector('.header-slider');
+  console.log('slidesData', slidesData);
+  console.log('ourGeneratedHTML', ourGeneratedHTML);
+  slidesContainer.innerHTML = ourGeneratedHTML;
+}
