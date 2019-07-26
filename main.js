0098a372cdbbb225f1764cc925a2e594b076bb5a
Pozharitskiy <33429207+Pozharitskiy@users.noreply.github.com>
2019-07-26 12:14:50 +0300

    Task solved

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-fetch('data.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((myJson) => {
-    console.log(myJson);
-    makePost(myJson);
-  });
+function morePosts() {
+  document.querySelector(".load-btn").style.display = "none";
+  document.querySelector(".loader").style.display = "block";
 
-function makePost(data) {
-  const sliderTemplate = document.getElementById('post').innerHTML;
-  const compiledTemplate = Handlebars.compile(sliderTemplate);
-  const ourGeneratedHTML = compiledTemplate(data);
-  const slidesContainer = document.querySelector('.posts-container');
-  slidesContainer.innerHTML = ourGeneratedHTML;
+
+  fetch('data.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      (myJson ? document.querySelector(".loader").style.display = "none" : console.log('error'));
+      document.querySelector(".load-btn").style.display = "block";
+      console.log(myJson);
+      makePost(myJson);
+    });
+
+  function makePost(data) {
+    const sliderTemplate = document.getElementById('post').innerHTML;
+    const compiledTemplate = Handlebars.compile(sliderTemplate);
+    const ourGeneratedHTML = compiledTemplate(data);
+    const slidesContainer = document.querySelector('.posts-container');
+    slidesContainer.innerHTML = ourGeneratedHTML;
+  }
+};
+
+function like() {
+  alert('#hireDen')
 }
+
+morePosts();
+
